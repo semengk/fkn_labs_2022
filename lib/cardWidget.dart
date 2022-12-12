e2408ba668e8cc4a75e99c0dda29c68599f5a0df
semengk <58079531+semengk@users.noreply.github.com>
2022-12-12 22:57:14 +0600

    Added info about heroes

--- lib/cardWidget.dart
+++ lib/cardWidget.dart
@@ -1,15 +1,18 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 class CardWidget extends StatelessWidget {
   const CardWidget({
     Key? key,
     required this.path,
     required this.hero,
+    required this.description,
   }) : super(key: key);
 
   final String path;
   final String hero;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
@@ -18,27 +21,38 @@ class CardWidget extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(14),
       ),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            path,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            bottom: 15,
-            right: 15,
-            left: 15,
-            child: Text(
-              hero,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 30,
-              ),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return DetailScreen(
+              path: path,
+              hero: hero,
+              description: description,
+            );
+          }));
+        },
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.network(
+              path,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            Positioned(
+              bottom: 15,
+              right: 15,
+              left: 15,
+              child: Text(
+                hero,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
